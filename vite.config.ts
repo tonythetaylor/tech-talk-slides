5595f7a70d0058abf16e51be06a33109d3e19c76
Anthony Taylor <ant.taylor2@gmail.com>
2025-07-20 15:05:57 -0400

    Prep for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+const repoName = 'tech-talk-slides'
+
 // https://vite.dev/config/
 export default defineConfig({
+  base: `/${repoName}/`,
   plugins: [react(), tailwindcss()],
 })
